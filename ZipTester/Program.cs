2aef884c20c8c9b81fc49b1a990d5c0a9cb28ba3
ty-n-42 <1196438+ty-n-42@users.noreply.github.com>
2023-04-07 16:48:15 +1000

    Find all files with .zip extension in the filename

--- ZipTester/Program.cs
+++ ZipTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,29 @@ namespace ZipTester
 {
     internal class Program
     {
+        private const string testPath = "C:\\Users\\tyn\\Downloads";
+        private const string zipExtension = "*.zip";
+
         static void Main(string[] args)
-        {
+        {   
+            // search the path for zip files
+            DirectoryInfo dInfo = new DirectoryInfo(testPath);
+            if (dInfo.Exists != true)
+            {
+                Console.WriteLine("ERROR: path does not exist: " + testPath);
+                Environment.Exit(-1);
+            }
+
+            // enumerate 
+            int count = 0;
+            foreach ( var fInfo in dInfo.EnumerateFiles(zipExtension, SearchOption.AllDirectories) )
+            {
+                count++;
+                Console.WriteLine("" + count + ": " + fInfo.FullName);
+            }
+
+            Console.WriteLine("finished!");
+            Console.ReadLine();
         }
     }
 }
